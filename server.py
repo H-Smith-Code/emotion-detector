7db85fd744589fc40bc578bbdbc6ef46d4e4c0dd
Hasani Smith <hasaniasmith@gmail.com>
2026-04-03 12:41:32 -0400

    Launched application

--- server.py
+++ server.py
@@ -13,7 +13,16 @@ def detector():
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detection(text_to_analyze)
-    return response
+
+    out_string = f"""For the given statement, the system response is
+                    'anger': {response['anger']}, 
+                    'disgust': {response['disgust']}, 
+                    'fear': {response['fear']}, 
+                    'joy': {response['joy']}, 
+                    and 'sadness': {response['sadness']}. 
+                    The dominant emotion is {response['dominant_emotion']}."""
+
+    return out_string
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
