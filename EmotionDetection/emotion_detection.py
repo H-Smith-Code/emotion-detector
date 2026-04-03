5739018c28d759a12a0aab7f9e4cb394fe0f348f
Hasani Smith <hasaniasmith@gmail.com>
2026-04-03 12:21:53 -0400

    Complete function and testing

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,4 +7,23 @@ def emotion_detection(text_to_analyse):  # Define a function named sentiment_ana
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = inputobj, headers=header)  # Send a POST request to the API with the text and headers
     response_dict = json.loads(response.text)
-    return response_dict  # Return the response text from the API
+
+    #Create dictionary to store emotion values
+    emotions_dict = {}
+    emotions_dict['anger'] = response_dict['emotionPredictions'][0]['emotion']['anger']
+    emotions_dict['disgust'] = response_dict['emotionPredictions'][0]['emotion']['disgust']
+    emotions_dict['fear'] = response_dict['emotionPredictions'][0]['emotion']['fear']
+    emotions_dict['joy'] = response_dict['emotionPredictions'][0]['emotion']['joy']
+    emotions_dict['sadness'] = response_dict['emotionPredictions'][0]['emotion']['sadness']
+    emotions_dict['dominant_emotion'] = 0.0
+
+    #Determine which emotion is dominant
+    emotion_list = list(emotions_dict.values())
+    emotion_list.sort()
+    dominant = emotion_list[-1]
+
+    for emotion in emotions_dict:
+        if emotions_dict[emotion] == dominant:
+            emotions_dict['dominant_emotion'] = emotion
+
+    return emotions_dict # Return the response text from the API
--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -5,18 +5,18 @@ class TestEmotionDetection(unittest.TestCase):
     def test_emotion_detector(self):
         # Test case for joy emotion
         result_1 = emotion_detection('I am glad this happened')
-        self.assertEqual(result_1['label'], 'joy')
+        self.assertEqual(result_1['dominant_emotion'], 'joy')
         # Test case for anger emotion
         result_2 = emotion_detection('I am really mad about this')
-        self.assertEqual(result_2['label'], 'anger')
+        self.assertEqual(result_2['dominant_emotion'], 'anger')
         # Test case for disgust emotion
         result_3 = emotion_detection('I feel disgusted just hearing about this')
-        self.assertEqual(result_3['label'], 'disgust')
+        self.assertEqual(result_3['dominant_emotion'], 'disgust')
         # Test case for sadness emotion
-        result_4 = emotion_detection('I feel disgusted just hearing about this')
-        self.assertEqual(result_4['label'], 'sadness')
+        result_4 = emotion_detection('I am so sad about this')
+        self.assertEqual(result_4['dominant_emotion'], 'sadness')
         # Test case for fear emotion
-        result_5 = emotion_detection('I feel disgusted just hearing about this')
-        self.assertEqual(result_5['label'], 'fear')
+        result_5 = emotion_detection('I am really afraid that this will happen')
+        self.assertEqual(result_5['dominant_emotion'], 'fear')
 
-unittest.main
+unittest.main()
